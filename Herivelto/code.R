c0022d83df6b241ec1bcc06684f3eacc09a46cbf
Antoine Soetewey <ant.soetewey@gmail.com>
2020-11-30 13:16:17 +0100

    Update code.R

--- Herivelto/code.R
+++ Herivelto/code.R
@@ -1,12 +1,19 @@
-dat <- matrix(c(63,78,94,65), ncol=2)
+dat <- matrix(c(63, 78, 94, 65),
+              ncol = 2)
 
 # 1st method:
 test1 <- chisq.test(dat,
-           correct = FALSE)
+  correct = FALSE
+)
 test1$p.value
 
 # 2nd method:
 test2 <- summary(as.table(dat))
 test2$p.value
 
-# both p-values are equal
+# 3rd method:
+library(vcd)
+test3 <- assocstats(dat)
+test3
+
+# all p-values are equal
